39e655e4362bff77b2e3555cf16ab6a32da9d116
Ben White <bwhite2140@outlook.com>
2020-06-05 10:31:10 -0700

    add branching in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,197 @@
 $(document).ready(function() {
-  $("#questionnaire").submit(function(event) {
+  $("form#questionnaire").submit(function(event) {
     event.preventDefault();
     const question1 = $("input:radio[name=question1]:checked").val();
     const question2 = $("input:radio[name=question2]:checked").val();
     const question3 = $("input:radio[name=question3]:checked").val();
     const question4 = $("input:radio[name=question4]:checked").val();
     const question5 = $("input:radio[name=question5]:checked").val();
-    
+    if (question3 === "constantly") {
+      if (question4 === "yes") {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").show();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").show();
+          $("#anything").hide();
+          $("#none").hide()
+        } else {
+          $("#java").show();
+          $("#c++").hide();
+          $("#python").show();
+          $("#anything").hide();
+          $("#none").hide()
+        }
+      } else if (question4 === "no") {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").show();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else {
+          $("#java").show();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        }
+      } else {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").show();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").show();
+          $("#none").hide()
+        }
+      }
+    } else if (question3 === "sometimes") {
+      if (question4 === "yes") {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").show();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").show();
+          $("#anything").hide();
+          $("#none").hide()
+        } else {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").show();
+          $("#anything").hide();
+          $("#none").hide()
+        }
+      } else if (question4 === "no") {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").show();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        }
+      } else {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").show();
+          $("#anything").hide();
+          $("#none").hide()
+        } else {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").show()
+        }
+      }
+    } else {
+      if (question4 === "yes") {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").show();
+          $("#anything").hide();
+          $("#none").hide()
+        } else {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").show();
+          $("#anything").hide();
+          $("#none").hide()
+        }
+      } else if (question4 === "no") {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").show()
+        } else {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        }
+      } else {
+        if (question5 === "yes") {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        } else if (question5 === "no") {
+          $("#java").hide();
+          $("#c++").hide();
+          $("#python").show();
+          $("#anything").hide();
+          $("#none").hide()
+        } else {
+          $("#java").hide();
+          $("#c++").show();
+          $("#python").hide();
+          $("#anything").hide();
+          $("#none").hide()
+        }
+      }
+    }
   });
 });
