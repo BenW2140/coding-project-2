e84da56de300d753317d7046314d0c8fc1e5438f
Ben White <bwhite2140@outlook.com>
2020-06-05 09:18:41 -0700

    add basic javascript functionality

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#questionnaire").submit(function(event) {
+    event.preventDefault();
+    const question1 = $("input:radio[name=question1]:checked").val();
+    const question2 = $("input:radio[name=question2]:checked").val();
+    const question3 = $("input:radio[name=question3]:checked").val();
+    const question4 = $("input:radio[name=question4]:checked").val();
+    const question5 = $("input:radio[name=question5]:checked").val();
+    
+  });
+});
