34e19f9c3b6ed1c679eb94663c039fa27e02ca9a
Ben White <bwhite2140@outlook.com>
2020-06-14 10:58:02 -0700

    dry up user logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,162 +2,76 @@ $(document).ready(function() {
   $("#questionnaire").submit(function(event) {
     event.preventDefault();
     const name = $("#name").val();
-    const question1 = $("input:radio[name=question1]:checked").val();
-    const question2 = $("input:radio[name=question2]:checked").val();
     const question3 = $("input:radio[name=question3]:checked").val();
     const question4 = $("input:radio[name=question4]:checked").val();
     const question5 = $("input:radio[name=question5]:checked").val();
     const question6 = $("input:radio[name=question6]:checked").val();
     $(".name").text(name);
+    $("#java").hide();
+    $("#c-plus-plus").hide();
+    $("#python").hide();
+    $("#c-sharp").hide();
+    $("#anything").hide();
+    $("#none").hide();
     if (question3 === "constantly") {
       if (question4 === "yes") {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").show();
-            $("#none").hide();
+            $("anything").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
             $("#anything").show();
-            $("#none").hide();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
             $("#python").show();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
             $("#anything").show();
-            $("#none").hide();
           }
         } else {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
             $("#anything").show();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
             $("#anything").show();
-            $("#none").hide();
           }
         }
       } else if (question4 === "no") {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").show();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-plus-plus").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
             $("#java").show();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
             $("#python").show();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           }
         } else {
           if (question6 === "insignificant") {
             $("#java").show();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         }
       } else {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").show();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-plus-plus").show();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
             $("#java").show();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           }
         } else {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").show();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-plus-plus").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
             $("#anything").show();
-            $("#none").hide();
           }
         }
       }
@@ -165,151 +79,61 @@ $(document).ready(function() {
       if (question4 === "yes") {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
             $("#python").show();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
             $("#anything").show();
-            $("#none").hide();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
             $("#java").show();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
             $("#python").show();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           }
         } else {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
             $("#python").show();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
             $("#anything").show();
-            $("#none").hide();
           }
         }
       } else if (question4 === "no") {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").show();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-plus-plus").show();
           } else {
             $("#java").show();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").show();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         } else {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
             $("#python").show();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           }
         }
       } else {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").show();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-plus-plus").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
             $("#java").show();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           }
         } else {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
             $("#python").show();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           }
         }
       }
@@ -318,150 +142,60 @@ $(document).ready(function() {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
             $("#java").show();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
             $("#python").show();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         } else {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         }
       } else if (question4 === "no") {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").show();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-plus-plus").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         } else {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         }
       } else {
         if (question5 === "yes") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").show();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-plus-plus").show();
           } else {
-            $("#java").hide();
-            $("#c++").show();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-plus-plus").show();
           }
         } else if (question5 === "no") {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         } else {
           if (question6 === "insignificant") {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").hide();
-            $("#anything").hide();
             $("#none").show();
           } else {
-            $("#java").hide();
-            $("#c++").hide();
-            $("#python").hide();
-            $("#c#").show();
-            $("#anything").hide();
-            $("#none").hide();
+            $("#c-sharp").show();
           }
         }
       }
